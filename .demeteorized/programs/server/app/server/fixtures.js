0e43391cb21a7825e57d0af5ebb292da64047a66
gaborpop <popgabor@gmail.com>
2014-01-16 14:33:31 +0000

    last commit

---
+++ .demeteorized/programs/server/app/server/fixtures.js
@@ -0,0 +1,26 @@
+(function(){if (Posts.find().count() === 0) {
+  Posts.insert({
+    title: 'initStratOui'
+    
+  });
+
+  Posts.insert({
+    title: 'initStratNon'
+    
+  });
+    
+  Posts.insert({
+    title: 'Messages',
+    content: "This is a first message"
+    
+  });
+  Posts.insert({
+    title: 'Messages',
+    content: "This is a second message"
+    
+  });
+
+  
+}
+
+})();
